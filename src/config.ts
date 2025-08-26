b6ad633a4ead1105ce1ef238c29770cc7fcfdedd
NgHoaiTan <tannh3108@gmail.com>
2025-08-26 15:26:55 +0700

    feature: add config mail transport

--- src/config.ts
+++ src/config.ts
@@ -16,6 +16,10 @@ class Config {
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
   public JWT_TOKEN: string | undefined;
+  public SENDER_EMAIL: string | undefined;
+  public SENDER_EMAIL_PASSWORD: string | undefined;
+  public SENDGRID_API_KEY: string | undefined;
+  public SENDGRID_SENDER: string | undefined;
 
   private readonly DEFAULT_DATABASE_URL = 'mongodb://root:123456@localhost:27018/';
 
@@ -31,6 +35,10 @@ class Config {
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
     this.JWT_TOKEN = process.env.JWT_TOKEN || '';
+    this.SENDER_EMAIL = process.env.SENDER_EMAIL || '';
+    this.SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD || '';
+    this.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
+    this.SENDGRID_SENDER = process.env.SENDGRID_SENDER || '';
   }
 
   public createLogger(name: string): bunyan {
