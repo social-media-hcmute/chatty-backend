289c72f41f7186343c30ddbd5e2ded792c437bdc
NgHoaiTan <tannh3108@gmail.com>
2025-08-27 23:31:04 +0700

    feature: add feature get all post

--- src/shared/services/db/post.service.ts
+++ src/shared/services/db/post.service.ts
@@ -10,6 +10,27 @@ class PostService {
     const user: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: 1 } });
     await Promise.all([post, user]);
   }
+
+  public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
+    let postQuery = {};
+    if (query?.imgId && query?.gifUrl) {
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' }}] };
+    } else {
+      postQuery = query;
+    }
+    const posts: IPostDocument[] = await PostModel.aggregate([
+      { $match: postQuery },
+      { $sort: sort },
+      { $skip: skip },
+      { $limit: limit }
+    ]);
+    return posts;
+  }
+
+  public async postsCount(): Promise<number> {
+    const count: number = await PostModel.find({}).countDocuments();
+    return count;
+  }
 }
 
 export const postService: PostService = new PostService();
