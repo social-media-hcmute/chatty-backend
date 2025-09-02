c709181cbe872d93062d96ce640400f20adf9dbc
NgHoaiTan <tannh3108@gmail.com>
2025-09-02 15:08:49 +0700

    feature: add feature update post

--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Create } from '@post/controllers/create-post';
 import { Delete } from '@post/controllers/delete-post';
 import { Get } from '@post/controllers/get-posts';
+import { Update } from '@post/controllers/update-post';
 import express, { Router } from 'express';
 class PostRoutes {
   private router: Router;
@@ -15,6 +16,9 @@ class PostRoutes {
 
     this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
+
+    this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.posts);
+     this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
 
     this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
 
