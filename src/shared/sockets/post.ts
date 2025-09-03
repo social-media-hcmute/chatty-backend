f0ca8005d3bfd0d67e06546c87f253c7749a3945
NgHoaiTan <tannh3108@gmail.com>
2025-09-04 00:13:59 +0700

    feature: add feature comment post

--- src/shared/sockets/post.ts
+++ src/shared/sockets/post.ts
@@ -1,3 +1,5 @@
+import { ICommentDocument } from '@comment/interfaces/comment.interface';
+import { IReactionDocument } from '@reaction/interfaces/reaction.interface';
 import {Server, Socket} from 'socket.io';
 
 export let socketIOPostObject: Server;
@@ -12,7 +14,13 @@ export class SocketIOPostHandler {
 
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
-      console.log('Post socketio handler');
+      socket.on('reaction', (reaction: IReactionDocument) => {
+        this.io.emit('update like', reaction);
+      })
+
+      socket.on('comment', (data: ICommentDocument) => {
+        this.io.emit('update comment', data);
+      })
     });
   }
 }
